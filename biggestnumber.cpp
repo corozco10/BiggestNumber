8f6882c4067a2358160ab0e526fe986e0e1c8847
corozco10 <corozco10@mail.csuchico.edu>
2018-02-06 11:44:30 -0800

    Comparing Numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,14 +6,22 @@ using namespace std; //so we don't need the prefix std::
 int main()
 {int number1;
 int number2;
-
+int biggestnumber;
   
   cout<<"Please enter a whole number:\n";
   cin>>number1;
-  
   cout<<"Please enter another whole number:\n";
   cin>>number2;
-  cout<<"Of those two numbers, the biggest is: ";
+  if(number1>number2)
+  {
+    biggestnumber=number1;
+  }
+    
+  else 
+  {  
+    biggestnumber=number2;
+  }
+  cout<<"Of those two numbers, the biggest is: "<<biggestnumber;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
